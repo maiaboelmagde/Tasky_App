7c1bf696fd5a540a57e5ce7074444e70ef4e5872
maiaboelmagd <maishalan00000@gmail.com>
2025-05-30 18:30:58 +0300

    refactor: extract darkTheme to seperated file

--- tasky_app/lib/main.dart
+++ tasky_app/lib/main.dart
@@ -3,6 +3,7 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:tasky_app/constants.dart';
 import 'package:tasky_app/screens/main_screen.dart';
 import 'package:tasky_app/screens/startup_screen.dart';
+import 'package:tasky_app/theme/dart_theme.dart';
 
 void main() async{
   WidgetsFlutterBinding.ensureInitialized(); 
@@ -17,87 +18,10 @@ class MyApp extends StatelessWidget {
   final String? userName;
   @override
   Widget build(BuildContext context) {
+    
     return MaterialApp(
       home: userName == null? StartupScreen():MainScreen(),
-      theme: ThemeData(
-        fontFamily:'Plus Jakarta Sans',
-        scaffoldBackgroundColor: Colors.black,
-        splashColor: Colors.black,
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ButtonStyle(
-            backgroundColor: WidgetStatePropertyAll(
-              Color.fromRGBO(21, 184, 108, 1),
-            ),
-            foregroundColor: WidgetStatePropertyAll(Colors.white),
-          ),
-        ),
-        textTheme: TextTheme(
-          displayLarge: TextStyle(
-            fontWeight: FontWeight.w400,
-            fontSize: 28,
-            color: Colors.white,
-          ),
-          displayMedium: TextStyle(
-            fontSize: 16,
-            color: Colors.white,
-          ),
-          displaySmall: TextStyle(
-            fontSize: 14,
-            color: Colors.white,
-          ),
-          bodySmall: TextStyle(
-            fontSize: 14,
-            color: Color.fromRGBO(198, 198, 198, 1),
-          ),
-          headlineLarge: TextStyle(
-            fontSize: 32,
-            color: Colors.white
-          )
-          
-        ),
-        inputDecorationTheme: InputDecorationTheme(
-          border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
-          enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: BorderSide.none,
-          ),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(16),
-            borderSide: BorderSide(color: Color.fromRGBO(21, 184, 108, 1))
-          ),
-          hintStyle: TextStyle(
-            fontSize: 16,
-            fontFamily: 'Plus Jakarta Sans',
-            color: Color.fromRGBO(109, 109, 109, 1),
-          ),
-          
-          filled: true,
-          fillColor: Color.fromRGBO(40, 40, 40, 1),
-        ),
-        floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Color.fromRGBO(21, 184, 108, 1),
-        ),
-        appBarTheme: AppBarTheme(
-          backgroundColor: Colors.black,
-          foregroundColor: Colors.white
-        ),
-
-        switchTheme: SwitchThemeData(
-          trackColor: WidgetStateProperty.resolveWith((status){
-            if(status.contains(WidgetState.selected)){
-              return (Color.fromRGBO(21, 184, 108, 1));
-            }
-            return null;
-          })
-        ),
-        bottomNavigationBarTheme:BottomNavigationBarThemeData(
-          backgroundColor: Color.fromRGBO(24, 24, 24, 1),
-          selectedItemColor: Color.fromRGBO(21, 184, 108, 1),
-          unselectedItemColor: Colors.white
-
-        )
-
-      ),
+      theme: darkTheme,
 
       debugShowCheckedModeBanner: false,
     );
---
+++ tasky_app/lib/theme/dart_theme.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+var darkTheme = ThemeData(
+  fontFamily: 'Plus Jakarta Sans',
+  scaffoldBackgroundColor: Colors.black,
+  splashColor: Colors.black,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ButtonStyle(
+      backgroundColor: WidgetStatePropertyAll(Color.fromRGBO(21, 184, 108, 1)),
+      foregroundColor: WidgetStatePropertyAll(Colors.white),
+    ),
+  ),
+  textTheme: TextTheme(
+    displayLarge: TextStyle(
+      fontWeight: FontWeight.w400,
+      fontSize: 28,
+      color: Colors.white,
+    ),
+    displayMedium: TextStyle(fontSize: 16, color: Colors.white),
+    displaySmall: TextStyle(fontSize: 14, color: Colors.white),
+    bodySmall: TextStyle(fontSize: 14, color: Color.fromRGBO(198, 198, 198, 1)),
+    headlineLarge: TextStyle(fontSize: 32, color: Colors.white),
+  ),
+  inputDecorationTheme: InputDecorationTheme(
+    border: OutlineInputBorder(borderRadius: BorderRadius.circular(16)),
+    enabledBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide.none,
+    ),
+    focusedBorder: OutlineInputBorder(
+      borderRadius: BorderRadius.circular(16),
+      borderSide: BorderSide(color: Color.fromRGBO(21, 184, 108, 1)),
+    ),
+    hintStyle: TextStyle(
+      fontSize: 16,
+      fontFamily: 'Plus Jakarta Sans',
+      color: Color.fromRGBO(109, 109, 109, 1),
+    ),
+
+    filled: true,
+    fillColor: Color.fromRGBO(40, 40, 40, 1),
+  ),
+  floatingActionButtonTheme: FloatingActionButtonThemeData(
+    backgroundColor: Color.fromRGBO(21, 184, 108, 1),
+  ),
+  appBarTheme: AppBarTheme(
+    backgroundColor: Colors.black,
+    foregroundColor: Colors.white,
+  ),
+
+  switchTheme: SwitchThemeData(
+    trackColor: WidgetStateProperty.resolveWith((status) {
+      if (status.contains(WidgetState.selected)) {
+        return (Color.fromRGBO(21, 184, 108, 1));
+      }
+      return null;
+    }),
+  ),
+  bottomNavigationBarTheme: BottomNavigationBarThemeData(
+    backgroundColor: Color.fromRGBO(24, 24, 24, 1),
+    selectedItemColor: Color.fromRGBO(21, 184, 108, 1),
+    unselectedItemColor: Colors.white,
+  ),
+);
